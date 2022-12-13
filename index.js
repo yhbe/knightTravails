1f26788eb276ef5cb11fba51ad60d45581b1052a
SHEF <Shefqetmush@gmail.com>
2022-12-13 07:01:53 -0500

    rename functions

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ class Graph {
   constructor() {
     return {
       board: null,
-      createChessBoard,
-      connectKnightMoves,
+      setVertices,
+      setPlayableKnightMoves,
       knightMove,
     };
-    function createChessBoard() {
+    function setVertices() {
       let map = new Map();
       for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
@@ -16,7 +16,7 @@ class Graph {
       return (this.board = map);
     }
 
-    function connectKnightMoves() {
+    function setPlayableKnightMoves() {
       for (let [elem] of this.board) {
         let splitElem = elem.split(",");
         let x = Number(splitElem[0]);
@@ -24,12 +24,12 @@ class Graph {
         let knightMoves = {
           1: [x + 1, y + 2],
           2: [x + 2, y + 1],
-          3: [x + 2, y + -1],
-          4: [x + 1, y - 2],
-          5: [x - 1, y + 2],
-          6: [x - 2, y + 2],
-          7: [x - 2, y + 1],
-          8: [x - 1, y + 2],
+          4: [x + 2, y - 1],
+          5: [x + 1, y - 2],
+          7: [x - 1, y - 2],
+          8: [x - 2, y - 1],
+          10: [x - 2, y + 1],
+          11: [x - 1, y + 2],
         };
         for (let number in knightMoves) {
           let move = knightMoves[number].toString();
@@ -54,7 +54,6 @@ class Graph {
       queue.push([start, [start]]);
       while (queue.length > 0) {
         let [active, path] = queue.shift();
-        console.log(`${active} active, path ${path}`);
         visited.add(active);
         if (active === end) {
           finishedPath.push(path);
@@ -62,6 +61,7 @@ class Graph {
         let possibleMoves = this.board.get(active);
         for (let elem of possibleMoves) {
           if (!visited.has(elem)) {
+            console.log(visited);
             queue.push([elem, [...path, elem]]);
           }
         }
@@ -72,7 +72,7 @@ class Graph {
   }
 }
 
-let chessMove = new Graph();
-chessMove.createChessBoard();
-chessMove.connectKnightMoves();
-chessMove.knightMove(["0,0"], ["5,4"]);
+let graph = new Graph();
+graph.setVertices();
+graph.setPlayableKnightMoves();
+graph.knightMove(["0,0"], ["1,2"]);
